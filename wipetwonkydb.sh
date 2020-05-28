f25c7d39904e761693b94489b69caba4c7a92149
Steven Mackenzie <disp-gh@aptile.co.uk>
2020-05-28 23:24:03 +0100

    Less aggressive DB wipe - leave thumbnails and user configuration

--- wipetwonkydb.sh
+++ wipetwonkydb.sh
@@ -3,17 +3,18 @@
 #trap read debug
 
 # Some twonkyserver.ini options only affect the collection of data, and have no 
-# effect if changed after a file has been found. If you change for example 
+# effect after metadata has been added to the db. 
 
-# This assumes that the twonky home directory and appdata options are /var/opt/twonky,
-# so that dbdir, tncache and the other working files are underneath in .twonky
+# This script assumes that the twonky home (or working) directory and appdata options 
+# are /var/opt/twonky, so that dbdir, tncache and the other working files are 
+# underneath .twonky
 
 # shutdown any running servers
 sudo systemctl stop twonky
 
 # remove the db and caches, leave the config file /etc/config/twonky/twonkyserver.ini
 # and license key /var/opt/twonky/.lynxtechnology/token in place
-sudo rm -rf /var/opt/twonky/.twonky/*
+sudo rm -rf /var/opt/twonky/.twonky/twonkyserver/db*
 
 # restart twonky, and it will start indexing based on the existing configuration
 sudo systemctl start twonky
