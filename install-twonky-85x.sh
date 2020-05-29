4ef9a509eb9531bc8e7e8f02f948a380b393b995
Steven <sjmac2000@gmail.com>
2020-05-30 00:01:23 +0100

    Default to armel download

--- install-twonky-85x.sh
+++ install-twonky-85x.sh
@@ -3,7 +3,8 @@
 #trap read debug
 
 TWONKY_DOWNLOAD=http://download.twonky.com/8.5.1/
-TWONKY_FILE=twonky-i686-glibc-2.9-8.5.1.zip
+#TWONKY_FILE=twonky-i686-glibc-2.9-8.5.1.zip
+TWONKY_FILE=twonky-armel-glibc-2.15-hf-8.5.1.zip
 
 echo Check if the Twonky zip package has been downloaded already
 if [ ! -e ./$TWONKY_FILE ]
@@ -56,8 +57,8 @@ sudo sh -c "echo \# Point twonky to the writable folders in this install >> /opt
 #sudo sh -c "echo cachedir=/var/opt/twonky/cache >> /opt/twonky/twonkyserver-default.ini"
 sudo sh -c "echo appdata=/var/opt/twonky >> /opt/twonky/twonkyserver-default.ini"
 
-echo Supply config changes to group tracks by albumartist in preference to artist
-sudo sh -c "echo \# Ensure twonky stores what it finds in the DB >> /opt/twonky/twonkyserver-default.ini"
+echo Supply config changes for the file scanner to help group tracks in the navigation
+sudo sh -c "echo \# Minimise unexpected metadata manipulations >> /opt/twonky/twonkyserver-default.ini"
 # Alternatively, or additionally: see view-patch.sh
 sudo sh -c "echo force_albumartist=0 >> /opt/twonky/twonkyserver-default.ini"
 sudo sh -c "echo scan_compilation_flag=2 >> /opt/twonky/twonkyserver-default.ini"
