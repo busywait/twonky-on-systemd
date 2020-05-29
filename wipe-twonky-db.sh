455eb064e3b3c3cbe01630d44695cff4acd947ff
Steven Mackenzie <disp-gh@aptile.co.uk>
2020-05-29 16:44:59 +0100

    Be more specific about which files to clean away

--- wipe-twonky-db.sh
+++ wipe-twonky-db.sh
@@ -9,12 +9,16 @@
 # are /var/opt/twonky, so that dbdir, tncache and the other working files are 
 # underneath .twonky
 
+echo This will stop the twonky service, remove the 
+
 # shutdown any running servers
 sudo systemctl stop twonky
 
 # remove the db and caches, leave the config file /etc/config/twonky/twonkyserver.ini
 # and license key /var/opt/twonky/.lynxtechnology/token in place
-sudo rm -rf /var/opt/twonky/.twonky/twonkyserver/db*
+sudo rm -rf /var/opt/twonky/.twonky/twonkyserver/db
+sudo rm -rf /var/opt/twonky/.twonky/twonkyserver/db.info
+sudo rm -rf /var/opt/twonky/.twonky/twonkyserver/db.info_bak
 
 # restart twonky, and it will start indexing based on the existing configuration
 sudo systemctl start twonky
