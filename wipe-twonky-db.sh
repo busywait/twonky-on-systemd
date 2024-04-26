3ddf05fa766a8ad3a246b48e7c4ac53c14d8f350
Steven Mackenzie <busywait@users.noreply.github.com>
2024-04-27 00:06:41 +0100

    Match paths to current install script and twonky version

--- wipe-twonky-db.sh
+++ wipe-twonky-db.sh
@@ -6,8 +6,9 @@
 # effect after metadata has been added to the db. 
 
 # This script assumes that the twonky home (or working) directory and appdata options 
-# are /var/opt/twonky, so that dbdir, tncache and the other working files are 
-# underneath .twonky
+# are /var/opt/twonky. In 8.5.2 dbdir, tncache and the other working files are 
+# directly underneath. Other configurations/earlier versions write these files 
+# in .twonky/twonkyserver.
 
 echo This will stop the twonky service, remove the metadata cache DBs and thumbnails. 
 
@@ -16,9 +17,9 @@ sudo systemctl stop twonky
 
 # remove the db and caches, leave the config file /etc/config/twonky/twonkyserver.ini
 # and license key /var/opt/twonky/.lynxtechnology/token in place
-sudo rm -rf /var/opt/twonky/.twonky/twonkyserver/db
-sudo rm -rf /var/opt/twonky/.twonky/twonkyserver/db.info
-sudo rm -rf /var/opt/twonky/.twonky/twonkyserver/db.info_bak
+sudo rm -rf /var/opt/twonky/db
+sudo rm -rf /var/opt/twonky/db.info
+sudo rm -rf /var/opt/twonky/db.info_bak
 
 # restart twonky, and it will start indexing based on the existing configuration
 sudo systemctl start twonky
